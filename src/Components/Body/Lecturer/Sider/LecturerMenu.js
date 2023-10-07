dc1236361ffe963c5b1e03773e550cce3811fa5f
Gia Khang <khanggia85@gmail.com>
2023-10-07 22:52:48 +0700

    add notification for requests in lecturer UI

--- src/Components/Body/Lecturer/Sider/LecturerMenu.js
+++ src/Components/Body/Lecturer/Sider/LecturerMenu.js
@@ -1,28 +1,64 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { Data } from "../../Body";
-import { Menu} from "antd";
+import { Menu, Badge } from "antd";
 import {
   DropboxOutlined,
   BellFilled,
   HomeFilled,
   SettingFilled,
-  EnvironmentFilled
+  EnvironmentFilled,
 } from "@ant-design/icons";
 import { LogOutBtn } from "../../../../ExtendedFunction/Users";
 
 export const LecturerMenu = () => {
   //Get data from app.js
   const { menuOpt, setMenuOpt, isDarkMode } = useContext(Data);
+  const [titleUpdate, setTitleUpdate] = useState(false);
 
   //Item for menu
   const menuItems = [
-    { label: "Home", icon:  <HomeFilled />, key: "lecturerDashboard" },
+    { label: "Home", icon: <HomeFilled />, key: "lecturerDashboard" },
     { label: "My slots", icon: <DropboxOutlined />, key: "createdSlot" },
-    { label: "Booking requests", icon: <BellFilled />, key: "request" },
-    { label: "Configurations", icon: <SettingFilled />, key: null, children: [{label: "Locations", icon: <EnvironmentFilled />, key: 'locations'},]}
+    {
+      label: "",
+      icon: (
+        <>
+          <Badge
+            size="small"
+            count={9}
+            style={Object.assign(
+              { position: "absolute" },
+              { bottom: 0 },
+              { right: "-20px" }
+            )}
+          />
+          <BellFilled />
+          <span style={Object.assign({ padding: "0 7px 0 0" })}>
+            Booking requests
+          </span>
+        </>
+      ),
+      key: "request",
+    },
+    {
+      label: "Configurations",
+      icon: <SettingFilled />,
+      key: null,
+      children: [
+        { label: "Locations", icon: <EnvironmentFilled />, key: "locations" },
+      ],
+    },
     // { label: "Configurations", icon: <ControlFilled />, null, [getItem('Locations', 'location')], "group" },
   ];
-  //selectedKeys
+  const originalTitle = "Meeting my Lecturers";
+  const updateTitle = () => {
+    titleUpdate
+      ? (document.title = "(9) Pending requests")
+      : (document.title = originalTitle);
+    setTitleUpdate(!titleUpdate);
+  };
+
+  setInterval(() => updateTitle(), 2000);
   return (
     <>
       <Menu
